34667a7400fdaf4f4b2e316a4caf84ed348ea9a2
echenim <myron.echenim@gmail.com>
2021-04-18 09:50:25 -0700

    created services with findall and save function

---
+++ services/stock-data-services.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"github.com/echenim/corelightfx/models"
+	"github.com/echenim/corelightfx/repository"
+)
+
+type StockService struct {
+	stockStoreRespository repository.StockStoreRespository
+}
+
+func ProviderStockService(s repository.StockStoreRespository) StockService {
+	return StockService{stockStoreRespository: s}
+}
+
+func (s *StockService) FindAll() []models.Stock {
+	return s.stockStoreRespository.FindAll()
+}
+
+func (s *StockService) Save(stock models.Stock) models.Stock {
+	s.stockStoreRespository.Save(stock)
+	return stock
+}
