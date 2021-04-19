855139135cd84adbfdb5f3edda970efd91ac4255
echenim <myron.echenim@gmail.com>
2021-04-19 02:33:58 -0700

    add private and public function for fetching stock data from provider and privide function to save the data to store

--- repository/stock-store.go
+++ repository/stock-store.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"time"
 
 	"github.com/echenim/corelightfx/models"
@@ -36,4 +39,139 @@ func (s *StockStoreRespository) Search(symbol string, t time.Time) []models.Stoc
 func (s *StockStoreRespository) Save(stock models.Stock) models.Stock {
 	s.DB.Save(&stock)
 	return stock
+}
+
+func (s *StockStoreRespository) GetFromProvider(symbol string, token string) {
+	s.getDataFromProvider(symbol, token)
+}
+
+func (s *StockStoreRespository) getDataFromProvider(symbol string, token string) {
+	var rs models.ProviderStockData
+	url := "https://sandbox.iexapis.com//stable/stock/" + symbol + "/quote?token=" + token
+
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	providerData, _ := ioutil.ReadAll(resp.Body)
+
+	var data models.ProviderStockData
+	json.Unmarshal(providerData, &data)
+
+	rs.Symbol = data.Symbol
+	rs.CompanyName = data.CompanyName
+	rs.PrimaryExchange = data.PrimaryExchange
+	rs.CalculationPrice = data.CalculationPrice
+	rs.Open = data.Open
+	rs.OpenTime = data.OpenTime
+	rs.OpenSource = data.OpenSource
+	rs.Close = data.Close
+	rs.CloseTime = data.CloseTime
+	rs.CloseSource = data.CloseSource
+	rs.High = data.High
+	rs.HighTime = data.HighTime
+	rs.HighSource = data.HighSource
+	rs.Low = data.Low
+	rs.LowTime = data.LowTime
+	rs.LowSource = data.LowSource
+	rs.LatestPrice = data.LatestPrice
+	rs.LatestSource = data.LatestSource
+	rs.LatestTime = data.LatestTime
+	rs.LatestUpdate = data.LatestUpdate
+	rs.LatestVolume = data.LatestVolume
+	rs.IexRealtimePrice = data.IexRealtimePrice
+	rs.IexRealtimeSize = data.IexRealtimeSize
+	rs.IexLastUpdated = data.IexLastUpdated
+	rs.DelayedPrice = data.DelayedPrice
+	rs.DelayedPriceTime = data.DelayedPriceTime
+	rs.OddLotDelayedPrice = data.OddLotDelayedPrice
+	rs.OddLotDelayedPriceTime = data.OddLotDelayedPriceTime
+	rs.ExtendedPrice = data.ExtendedPrice
+	rs.ExtendedChange = data.ExtendedChange
+	rs.ExtendedChangePercent = data.ExtendedChangePercent
+	rs.ExtendedPriceTime = data.ExtendedPriceTime
+	rs.PreviousClose = data.PreviousClose
+	rs.PreviousVolume = data.PreviousVolume
+	rs.Change = data.Change
+	rs.ChangePercent = data.ChangePercent
+	rs.Volume = data.Volume
+	rs.IexMarketPercent = data.IexMarketPercent
+	rs.IexVolume = data.IexVolume
+	rs.AvgTotalVolume = data.AvgTotalVolume
+	rs.IexBidPrice = data.IexBidPrice
+	rs.IexBidSize = data.IexBidSize
+	rs.IexAskPrice = data.IexAskPrice
+	rs.IexAskSize = data.IexAskSize
+	rs.IexOpen = data.IexOpen
+	rs.IexOpenTime = data.IexOpenTime
+	rs.IexClose = data.IexClose
+	rs.IexCloseTime = data.IexCloseTime
+	rs.MarketCap = data.MarketCap
+	rs.PeRatio = data.PeRatio
+	rs.Week52High = data.Week52High
+	rs.Week52Low = data.Week52Low
+	rs.YtdChange = data.YtdChange
+	rs.LastTradeTime = data.LastTradeTime
+	rs.IsUSMarketOpen = data.IsUSMarketOpen
+	s.save(rs)
+}
+
+func (s *StockStoreRespository) save(data models.ProviderStockData) {
+	stock := models.Stock{
+		Symbol:                 data.Symbol,
+		CompanyName:            data.CompanyName,
+		PrimaryExchange:        data.PrimaryExchange,
+		CalculationPrice:       data.CalculationPrice,
+		Open:                   data.Open,
+		OpenTime:               data.OpenTime,
+		OpenSource:             data.OpenSource,
+		Close:                  data.Close,
+		CloseTime:              data.CloseTime,
+		CloseSource:            data.CloseSource,
+		High:                   data.High,
+		HighTime:               data.HighTime,
+		HighSource:             data.HighSource,
+		Low:                    data.Low,
+		LowTime:                data.LowTime,
+		LowSource:              data.LowSource,
+		LatestPrice:            data.LatestPrice,
+		LatestSource:           data.LatestSource,
+		LatestTime:             data.LatestTime,
+		LatestUpdate:           data.LatestUpdate,
+		LatestVolume:           data.LatestVolume,
+		IexRealtimePrice:       data.IexRealtimePrice,
+		IexRealtimeSize:        data.IexRealtimeSize,
+		IexLastUpdated:         data.IexLastUpdated,
+		DelayedPrice:           data.DelayedPrice,
+		DelayedPriceTime:       data.DelayedPriceTime,
+		OddLotDelayedPrice:     data.OddLotDelayedPrice,
+		OddLotDelayedPriceTime: data.OddLotDelayedPriceTime,
+		ExtendedPrice:          data.ExtendedPrice,
+		ExtendedChange:         data.ExtendedChange,
+		ExtendedChangePercent:  data.ExtendedChangePercent,
+		ExtendedPriceTime:      data.ExtendedPriceTime,
+		PreviousClose:          data.PreviousClose,
+		PreviousVolume:         data.PreviousVolume,
+		Change:                 data.Change,
+		ChangePercent:          data.ChangePercent,
+		Volume:                 data.Volume,
+		IexMarketPercent:       data.IexMarketPercent,
+		IexVolume:              data.IexVolume,
+		AvgTotalVolume:         data.AvgTotalVolume,
+		IexBidPrice:            data.IexBidPrice,
+		IexBidSize:             data.IexBidSize,
+		IexAskPrice:            data.IexAskPrice,
+		IexAskSize:             data.IexAskSize,
+		IexOpen:                data.IexOpen,
+		IexOpenTime:            data.IexOpenTime,
+		IexClose:               data.IexClose,
+		IexCloseTime:           data.IexCloseTime,
+		MarketCap:              data.MarketCap,
+		PeRatio:                data.PeRatio,
+		Week52High:             data.Week52High,
+		Week52Low:              data.Week52Low,
+		YtdChange:              data.YtdChange,
+		LastTradeTime:          data.LastTradeTime,
+		IsUSMarketOpen:         data.IsUSMarketOpen,
+		Stamp:                  time.Now(),
+	}
+	s.DB.Save(&stock)
 }
