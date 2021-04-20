bedf8dc6350abb4ddad07a2f0dae653206bf8f81
echenim <myron.echenim@gmail.com>
2021-04-19 20:44:44 -0700

    function to read config from file

---
+++ utilities/read-config.go
@@ -0,0 +1,16 @@
+package utilities
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+func GetConfig() map[string]interface{} {
+	j, _ := os.Open("/utilities/config.json")
+	defer j.Close()
+	b, _ := io.ReadAll(j)
+	var settings map[string]interface{}
+	json.Unmarshal([]byte(b), &settings)
+	return settings
+}
